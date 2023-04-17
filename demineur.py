9ac5122d25549cf6a04815f536624e423eac985c
Ulfangur <130994158+Ulfangur@users.noreply.github.com>
2023-04-17 14:52:32 +0200

    Fichier Démineur debut
    Création d'un tableau de jeu, avec génération des bombes 
    et le nombre de bombes a un point x y

---
+++ demineur.py
@@ -0,0 +1,111 @@
+from random import randint
+class Grille:
+    def __init__(self):
+        self.taille = 16
+        self.tableau_jeu = [[0 for i in range(self.taille)]for j in range(self.taille)]
+
+    def affichage(self):
+        for i in range(self.taille):
+            chaine:str = ""
+            for j in range(self.taille):
+                chaine = chaine + " " + str(self.tableau_jeu[i][j])
+            print(chaine, "\n")
+
+    def tableau_mine_init(self):
+        nombre_bombes:int = 50
+        x:int = 0
+        y:int = 0
+        unefois:bool = True
+        for i in range(nombre_bombes):
+            while (self.tableau_jeu[x][y] == -1) or (unefois):
+                x = randint(0,self.taille-1)  
+                y = randint(0,self.taille-1) 
+                unefois = False
+            self.tableau_jeu[x][y] = -1
+            if (x==0):
+                if (y==0):
+                    if (self.tableau_jeu[0][1] != -1):
+                        self.tableau_jeu[0][1] = self.tableau_jeu[0][1] + 1 
+                    if (self.tableau_jeu[1][1] != -1):
+                        self.tableau_jeu[1][1] = self.tableau_jeu[1][1] + 1 
+                    if (self.tableau_jeu[1][0] != -1):
+                        self.tableau_jeu[1][0] = self.tableau_jeu[1][0] + 1 
+                elif (y==self.taille-1):
+                    if (self.tableau_jeu[1][self.taille-1] != -1):
+                        self.tableau_jeu[1][self.taille-1] = self.tableau_jeu[1][self.taille-1] + 1 
+                    if (self.tableau_jeu[1][self.taille-2] != -1):
+                        self.tableau_jeu[1][self.taille-2] = self.tableau_jeu[1][self.taille-2] + 1 
+                    if (self.tableau_jeu[0][self.taille-2] != -1):
+                        self.tableau_jeu[0][self.taille-2] = self.tableau_jeu[0][self.taille-2] + 1 
+                else:
+                    if (self.tableau_jeu[0][y-1] != -1):
+                        self.tableau_jeu[0][y-1] = self.tableau_jeu[0][y-1] + 1
+                    if (self.tableau_jeu[0][y+1] != -1):
+                        self.tableau_jeu[0][y+1] = self.tableau_jeu[0][y+1] +1
+                    if (self.tableau_jeu[1][y-1] != -1):
+                        self.tableau_jeu[1][y-1] = self.tableau_jeu[1][y-1] +1
+                    if (self.tableau_jeu[1][y] != -1):
+                        self.tableau_jeu[1][y] = self.tableau_jeu[1][y] +1
+                    if (self.tableau_jeu[1][y+1] != -1):
+                        self.tableau_jeu[1][y+1] = self.tableau_jeu[1][y+1] +1
+            elif (x == self.taille-1):
+                if (y==0):
+                    if (self.tableau_jeu[self.taille-1][1] != -1):
+                        self.tableau_jeu[self.taille-1][1] = self.tableau_jeu[self.taille-1][1] + 1 
+                    if (self.tableau_jeu[self.taille-2][1] != -1):
+                        self.tableau_jeu[self.taille-2][1] = self.tableau_jeu[self.taille-2][1] + 1 
+                    if (self.tableau_jeu[self.taille-2][0] != -1):
+                        self.tableau_jeu[self.taille-2][0] = self.tableau_jeu[self.taille-2][0] + 1 
+                elif (y==self.taille-1):
+                    if (self.tableau_jeu[self.taille-2][self.taille-1] != -1):
+                        self.tableau_jeu[self.taille-2][self.taille-1] = self.tableau_jeu[self.taille-2][self.taille-1] + 1 
+                    if (self.tableau_jeu[self.taille-2][self.taille-2] != -1):
+                        self.tableau_jeu[self.taille-2][self.taille-2] = self.tableau_jeu[self.taille-2][self.taille-2] + 1 
+                    if (self.tableau_jeu[self.taille-1][self.taille-2] != -1):
+                        self.tableau_jeu[self.taille-1][self.taille-2] = self.tableau_jeu[0][self.taille-2] + 1 
+                else:
+                    if (self.tableau_jeu[self.taille-1][y-1] != -1):
+                        self.tableau_jeu[self.taille-1][y-1] = self.tableau_jeu[self.taille-1][y-1] + 1
+                    if (self.tableau_jeu[self.taille-1][y+1] != -1):
+                        self.tableau_jeu[self.taille-1][y+1] = self.tableau_jeu[self.taille-1][y+1] +1
+                    if (self.tableau_jeu[self.taille-2][y-1] != -1):
+                        self.tableau_jeu[self.taille-2][y-1] = self.tableau_jeu[self.taille-2][y-1] +1
+                    if (self.tableau_jeu[self.taille-2][y] != -1):
+                        self.tableau_jeu[self.taille-2][y] = self.tableau_jeu[self.taille-2][y] +1
+                    if (self.tableau_jeu[self.taille-2][y+1] != -1):
+                        self.tableau_jeu[self.taille-2][y+1] = self.tableau_jeu[self.taille-2][y+1] +1
+            elif (y == 0):
+                if (self.tableau_jeu[][] != -1):
+                    self.tableau_jeu[][] = self.tableau_jeu[][] + 1
+                if (self.tableau_jeu[][] != -1):
+                    self.tableau_jeu[][] = self.tableau_jeu[][] + 1
+                if (self.tableau_jeu[][] != -1):
+                    self.tableau_jeu[][] = self.tableau_jeu[][] + 1
+                if (self.tableau_jeu[][] != -1):
+                    self.tableau_jeu[][] = self.tableau_jeu[][] + 1
+                if (self.tableau_jeu[][] != -1):
+                    self.tableau_jeu[][] = self.tableau_jeu[][] + 1
+            print(x, " ", y, " ", i)
+            unefois = True
+
+def verification(Tab):
+    """
+    Permet de vérifier si le tableau est bien crée avec 50 bombes.
+    Arguement :
+        Tab : Argument de type Grille.
+    Return : rien .
+    Print: affiche le nombre de bombes dans la console.
+    """
+    compteur:int = 0
+    for i in range(Tab.taille):
+        for j in range(Tab.taille):
+            if (Tab.tableau_jeu[i][j] == -1):
+                compteur = compteur + 1
+    print(compteur)    
+    
+    
+Test = Grille()
+Test.affichage()
+Test.tableau_mine_init()
+Test.affichage()
+#verification(Test)
