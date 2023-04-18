3d4be54a41952a9d9f4b25613ea20ccc11b23728
Ulfangur <ulfangur@gmail.com>
2023-04-18 11:29:03 +0200

    Division de la grande fonction qui génère les numéros autour des bombes. Le programme objet est maintenant plus divisé.

--- demineur.py
+++ demineur.py
@@ -36,53 +36,11 @@ class Grille:
             if (x==0):
                 self.cas_haut(x,y)
             elif (x == self.taille-1):
-                if (y==0):
-                    if (self.tableau_jeu[self.taille-1][1] != -1):
-                        self.tableau_jeu[self.taille-1][1] = self.tableau_jeu[self.taille-1][1] + 1 
-                    if (self.tableau_jeu[self.taille-2][1] != -1):
-                        self.tableau_jeu[self.taille-2][1] = self.tableau_jeu[self.taille-2][1] + 1 
-                    if (self.tableau_jeu[self.taille-2][0] != -1):
-                        self.tableau_jeu[self.taille-2][0] = self.tableau_jeu[self.taille-2][0] + 1 
-                elif (y==self.taille-1):
-                    if (self.tableau_jeu[self.taille-2][self.taille-1] != -1):
-                        self.tableau_jeu[self.taille-2][self.taille-1] = self.tableau_jeu[self.taille-2][self.taille-1] + 1 
-                    if (self.tableau_jeu[self.taille-2][self.taille-2] != -1):
-                        self.tableau_jeu[self.taille-2][self.taille-2] = self.tableau_jeu[self.taille-2][self.taille-2] + 1 
-                    if (self.tableau_jeu[self.taille-1][self.taille-2] != -1):
-                        self.tableau_jeu[self.taille-1][self.taille-2] = self.tableau_jeu[0][self.taille-2] + 1 
-                else:
-                    if (self.tableau_jeu[self.taille-1][y-1] != -1):
-                        self.tableau_jeu[self.taille-1][y-1] = self.tableau_jeu[self.taille-1][y-1] + 1
-                    if (self.tableau_jeu[self.taille-1][y+1] != -1):
-                        self.tableau_jeu[self.taille-1][y+1] = self.tableau_jeu[self.taille-1][y+1] +1
-                    if (self.tableau_jeu[self.taille-2][y-1] != -1):
-                        self.tableau_jeu[self.taille-2][y-1] = self.tableau_jeu[self.taille-2][y-1] +1
-                    if (self.tableau_jeu[self.taille-2][y] != -1):
-                        self.tableau_jeu[self.taille-2][y] = self.tableau_jeu[self.taille-2][y] +1
-                    if (self.tableau_jeu[self.taille-2][y+1] != -1):
-                        self.tableau_jeu[self.taille-2][y+1] = self.tableau_jeu[self.taille-2][y+1] +1
+                self.cas_bas(x,y)
             elif (y == 0):
-                if (self.tableau_jeu[x-1][y] != -1):
-                    self.tableau_jeu[x-1][y] = self.tableau_jeu[x-1][y] + 1
-                if (self.tableau_jeu[x-1][y+1]!= -1):
-                    self.tableau_jeu[x-1][y+1] = self.tableau_jeu[x-1][y+1] + 1
-                if (self.tableau_jeu[x][y+1] != -1):
-                    self.tableau_jeu[x][y+1] = self.tableau_jeu[x][y+1] + 1
-                if (self.tableau_jeu[x+1][y] != -1):
-                    self.tableau_jeu[x+1][y] = self.tableau_jeu[x+1][y] + 1
-                if (self.tableau_jeu[x+1][y+1] != -1):
-                    self.tableau_jeu[x+1][y+1] = self.tableau_jeu[x+1][y+1] + 1
+                self.cas_gauche(x,y)
             elif (y == self.taille-1):
-                if (self.tableau_jeu[x-1][self.taille-2] != -1):
-                    self.tableau_jeu[x-1][self.taille-2] = self.tableau_jeu[x-1][self.taille-2] + 1
-                if (self.tableau_jeu[x-1][self.taille-1]!= -1):
-                    self.tableau_jeu[x-1][self.taille-1] = self.tableau_jeu[x-1][self.taille-1] + 1
-                if (self.tableau_jeu[x][self.taille-2] != -1):
-                    self.tableau_jeu[x][self.taille-2] = self.tableau_jeu[x][self.taille-2] + 1
-                if (self.tableau_jeu[x+1][self.taille-2] != -1):
-                    self.tableau_jeu[x+1][self.taille-2] = self.tableau_jeu[x+1][self.taille-2] + 1
-                if (self.tableau_jeu[x+1][self.taille-1] != -1):
-                    self.tableau_jeu[x+1][self.taille-1] = self.tableau_jeu[x+1][self.taille-1] + 1
+                self.cas_droite(x,y)
             else:
                 self.cas_normal(x,y)
             unefois = True
@@ -121,6 +79,66 @@ class Grille:
             self.cas_coin_haut_droite(x,y)
         else:
             self.cas_haut_longueur(x,y)
+
+    def cas_coin_bas_gauche(self,x,y):
+        if (self.tableau_jeu[self.taille-1][1] != -1):
+            self.tableau_jeu[self.taille-1][1] = self.tableau_jeu[self.taille-1][1] + 1 
+        if (self.tableau_jeu[self.taille-2][1] != -1):
+            self.tableau_jeu[self.taille-2][1] = self.tableau_jeu[self.taille-2][1] + 1 
+        if (self.tableau_jeu[self.taille-2][0] != -1):
+            self.tableau_jeu[self.taille-2][0] = self.tableau_jeu[self.taille-2][0] + 1
+
+    def cas_coin_bas_droite(self,x,y):
+        if (self.tableau_jeu[self.taille-2][self.taille-1] != -1):
+            self.tableau_jeu[self.taille-2][self.taille-1] = self.tableau_jeu[self.taille-2][self.taille-1] + 1 
+        if (self.tableau_jeu[self.taille-2][self.taille-2] != -1):
+            self.tableau_jeu[self.taille-2][self.taille-2] = self.tableau_jeu[self.taille-2][self.taille-2] + 1 
+        if (self.tableau_jeu[self.taille-1][self.taille-2] != -1):
+            self.tableau_jeu[self.taille-1][self.taille-2] = self.tableau_jeu[0][self.taille-2] + 1
+
+    def cas_bas_longueur(self,x,y):
+        if (self.tableau_jeu[self.taille-1][y-1] != -1):
+            self.tableau_jeu[self.taille-1][y-1] = self.tableau_jeu[self.taille-1][y-1] + 1
+        if (self.tableau_jeu[self.taille-1][y+1] != -1):
+            self.tableau_jeu[self.taille-1][y+1] = self.tableau_jeu[self.taille-1][y+1] +1
+        if (self.tableau_jeu[self.taille-2][y-1] != -1):
+            self.tableau_jeu[self.taille-2][y-1] = self.tableau_jeu[self.taille-2][y-1] +1
+        if (self.tableau_jeu[self.taille-2][y] != -1):
+            self.tableau_jeu[self.taille-2][y] = self.tableau_jeu[self.taille-2][y] +1
+        if (self.tableau_jeu[self.taille-2][y+1] != -1):
+            self.tableau_jeu[self.taille-2][y+1] = self.tableau_jeu[self.taille-2][y+1] +1
+
+    def cas_bas(self,x,y):
+        if (y==0):
+            self.cas_coin_bas_gauche(x,y)
+        elif (y==self.taille-1):
+            self.cas_coin_bas_droite(x,y)
+        else:
+            self.cas_bas_longueur
+
+    def cas_gauche(self,x,y):
+        if (self.tableau_jeu[x-1][y] != -1):
+            self.tableau_jeu[x-1][y] = self.tableau_jeu[x-1][y] + 1
+        if (self.tableau_jeu[x-1][y+1]!= -1):
+            self.tableau_jeu[x-1][y+1] = self.tableau_jeu[x-1][y+1] + 1
+        if (self.tableau_jeu[x][y+1] != -1):
+            self.tableau_jeu[x][y+1] = self.tableau_jeu[x][y+1] + 1
+        if (self.tableau_jeu[x+1][y] != -1):
+            self.tableau_jeu[x+1][y] = self.tableau_jeu[x+1][y] + 1
+        if (self.tableau_jeu[x+1][y+1] != -1):
+            self.tableau_jeu[x+1][y+1] = self.tableau_jeu[x+1][y+1] + 1
+
+    def cas_droite(self,x,y):
+        if (self.tableau_jeu[x-1][self.taille-2] != -1):
+            self.tableau_jeu[x-1][self.taille-2] = self.tableau_jeu[x-1][self.taille-2] + 1
+        if (self.tableau_jeu[x-1][self.taille-1]!= -1):
+            self.tableau_jeu[x-1][self.taille-1] = self.tableau_jeu[x-1][self.taille-1] + 1
+        if (self.tableau_jeu[x][self.taille-2] != -1):
+            self.tableau_jeu[x][self.taille-2] = self.tableau_jeu[x][self.taille-2] + 1
+        if (self.tableau_jeu[x+1][self.taille-2] != -1):
+            self.tableau_jeu[x+1][self.taille-2] = self.tableau_jeu[x+1][self.taille-2] + 1
+        if (self.tableau_jeu[x+1][self.taille-1] != -1):
+            self.tableau_jeu[x+1][self.taille-1] = self.tableau_jeu[x+1][self.taille-1] + 1
 
     def cas_normal (self,x,y):
         if(self.tableau_jeu[x-1][y-1] != -1):
